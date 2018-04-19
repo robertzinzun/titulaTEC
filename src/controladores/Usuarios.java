f14b5996087ab900d07674c0d67dca3ebbadc3cc
robertzinzun <robertzinzun@gmail.com>
2018-04-18 21:46:47 -0500

    Revert "configuracion"
    This reverts commit 877d57edb9923e1f0aa4befeaffef29fa56bef29.

--- src/controladores/Usuarios.java
+++ src/controladores/Usuarios.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Usuarios
  */
-//@WebServlet(name="usuarios",urlPatterns="/Usuarios")
+@WebServlet(name="usuarios",urlPatterns="/Usuarios")
 public class Usuarios extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
